09f1374d8cd0434d25f87234bbe7ca2765064fd9
zahid-rizwan <zahidibnrizwan@gmai.com>
2024-12-16 00:10:14 +0530

    SecurityConfis created

--- study-progress-tracker/src/main/java/com/sps/config/SecurityConfig.java
+++ study-progress-tracker/src/main/java/com/sps/config/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.sps.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(customizer->customizer.disable())
@@ -18,11 +28,28 @@ public class SecurityConfig {
 //        .formLogin(Customizer.withDefaults())
         .httpBasic(Customizer.withDefaults())
         .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-
-
-
-
         return http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1= User
+                .withDefaultPasswordEncoder()
+                .username("zahid")
+                .password("1234")
+                .roles("USER")
+                .build();
+        UserDetails user2= User
+                .withDefaultPasswordEncoder()
+                .username("hibban")
+                .password("1234")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1,user2);
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsService);
     }
 }
